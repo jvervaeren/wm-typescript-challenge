49bbd80549ff86eb93cb92487a8c7f939a29a8cd
jens-vervaeren <vervaeren.jens@gmail.com>
2025-10-09 12:58:30 +0200

    Feat: Added title to all cocktails page

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -3,6 +3,7 @@ import { AllCocktails } from "../components/allCocktails"
 export default function RecipePage() {
   return (
     <main>
+      <h2 className="text-4xl text-gray-900 font-bold py-8">All cocktails</h2>
       <ul className="grid grid-cols-1 auto-rows-max gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <AllCocktails />
       </ul>

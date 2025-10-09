53a786cc678dde12261ef5c8e8d124a6fb291891
jens-vervaeren <vervaeren.jens@gmail.com>
2025-10-09 20:42:00 +0200

    refactor: small changes to cocktail grid on homepage

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -10,16 +10,16 @@ interface CardProps {
           </span> */}
 
 export const Card = ({ name, type, ingredients, preparation }: CardProps) => {
-  const ingredientList = ingredients.map(ingredient => <li>{ingredient}</li>)
+  const ingredientList = ingredients.map(ingredient => <li><span className="text-base text-gray-900">{ingredient}</span></li>)
 
   return (
     <div className="flex bg-white border border-gray-400 rounded-md p-4 h-full" tabIndex={0}>
       <div className="flex flex-col">
         <div className="pb-4">
-          <h2 className="text-2xl font-bold">{name}</h2>
+          <h2 className="text-2xl text-gray-900 font-bold">{name}</h2>
         </div>
         <div className="pb-4">
-          <h3 className="text-xl font-medium">Ingredients</h3>
+          <h3 className="text-xl text-gray-900 font-medium">Ingredients</h3>
           <ul
             className="list-inside list-[square] px-1 text-base overflow-auto min-h-24 max-h-24 pr-1"
             aria-label="Scrollable ingredients list"
@@ -30,9 +30,9 @@ export const Card = ({ name, type, ingredients, preparation }: CardProps) => {
         </div>
 
         <div>
-          <h3 className="text-xl font-medium">Preparation</h3>
+          <h3 className="text-xl text-gray-900 font-medium">Preparation</h3>
           <p
-            className="text-base overflow-auto min-h-24 max-h-24 pr-1"
+            className="text-base text-gray-900 overflow-auto min-h-24 max-h-24 pr-1"
             tabIndex={0}
           >
             {preparation}
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,20 @@ import { PopularCocktails } from "./components/popularCocktails"
 export default function Home() {
   return (
     <main>
-      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <PopularCocktails />
-      </ul>
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-12">
+        <div className="col-span-1">
+          <h1 className="text-5xl text-gray-900 font-bold py-8">Welcome!</h1>
+          <p className="text-base text-gray-900 pb-6">
+            Welcome to Wieni's cocktail application. A database of all of your colleagues favourite cocktails. We hope you enjoy em as much as we do.
+          </p>
+          <p className="text-base text-gray-900">On this page, you can already find five of our highest ranked cocktails between colleagues.</p>
+        </div>
+        <div className="col-span-2">
+          <ul className="grid grid-cols-1 gap-4 py-8 sm:grid-cols-2 lg:grid-cols-3">
+            <PopularCocktails />
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }

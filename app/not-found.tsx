917c912c4f7d940a495e2eb2c02186c59f16fe17
jens-vervaeren <vervaeren.jens@gmail.com>
2025-10-09 21:58:17 +0200

    WCA-002: fix - added some spacing and correct semantic tag

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 export default function NotFound() {
 	return (
 		<main>
-			<h2 className="text-4xl text-center text-gray-900 font-bold">
-				Why is the rum gone?
-				<span className="sr-only">Page not found</span>
-			</h2>
-			<p className="block text-center text-gray-900 py-8">
-				The requested page you were looking for could not be found
-			</p>
-			<p className="block text-center">
-				<Link
-					href="/"
-					className="p-2 underline underline-offset-4 decoration-gray-900 text-gray-900 rounded-sm hover:bg-gray-900 hover:text-gray-50 focus:bg-gray-900 focus:text-gray-50"
-				>
-					To homepage
-					<span className="sr-only">Go to homepage</span>
-				</Link>
-			</p>
+			<div className="flex flex-col">
+				<h1 className="text-4xl text-center text-gray-900 font-bold py-12">
+					Why is the rum gone?
+					<span className="sr-only">Page not found</span>
+				</h1>
+				<p className="block text-center text-gray-900 py-8">
+					The requested page you were looking for could not be found
+				</p>
+				<p className="block text-center">
+					<Link
+						href="/"
+						className="p-2 underline underline-offset-4 decoration-gray-900 text-gray-900 rounded-sm hover:bg-gray-900 hover:text-gray-50 focus:bg-gray-900 focus:text-gray-50"
+					>
+						To homepage
+						<span className="sr-only">Go to homepage</span>
+					</Link>
+				</p>
+			</div>
 		</main>
 	);
 }
